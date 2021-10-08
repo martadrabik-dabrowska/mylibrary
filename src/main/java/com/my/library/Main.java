b350f8a2511740d64d1e5e8af8ae1772dbf1cd9b
martadrabik-dabrowska <m.drabik.dabrowska@gmail.com>
2021-10-09 00:39:22 +0200

    Creation Resource bundles

--- src/main/java/com/my/library/Main.java
+++ src/main/java/com/my/library/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -15,11 +18,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Locale.setDefault(new Locale("en"));
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(resourceBundle);
         BorderPane borderPane = loader.load();
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("My Library");
+        primaryStage.setTitle(resourceBundle.getString("title.application"));
         primaryStage.show();
 
     }
